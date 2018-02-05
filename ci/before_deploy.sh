ceca86288e634c5f5cf6bb02a446ca9629805ed0
Sean Cross <sean@xobs.io>
2018-02-05 11:49:39 +0800

    ci: add .exe to before_deploy
    Windows programs end in .exe.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -21,7 +21,7 @@ main() {
     cross rustc --bin miditran --target $TARGET --release -- -C lto
 
     # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/miditran $stage/
+    cp target/$TARGET/release/miditran.exe $stage/
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
